c536c64bf5c93927880b9100cb5898632de4e363
Vito <vitopensapiccolo@gmail.com>
2024-01-07 00:41:01 -0300

    joystick tested on raspi

--- main_test2.c
+++ main_test2.c
@@ -11,90 +11,108 @@
 #define SENSITIVITY 50
 /**************************************************************************/
 
+#include "tetramino.h"
+#include "board.h"
+#include "game.h"
+#include "joydrv.h"
+#include "raspi.h"
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+#include <unistd.h>
+
+#define SENSITIVITY 50
+/**************************************************************************/
+
 int main(void)
 {
-	int fallInterval = 1000; // 1 seg
-	time_t startTime = time(NULL);
+        int fallInterval = 1000; // 1 seg
+        time_t startTime = time(NULL);
 
-	srand((unsigned int)time(NULL)); // seed para rand()
+        srand((unsigned int)time(NULL)); // seed para rand()
 
-	player_t player;
-	initGame(&player);
-	joy_init();
-	joyinfo_t joystick = {0, 0, J_NOPRESS};
+        player_t player;
+        initGame(&player);
 
-	do
-	{
-		// control
-		char key = whichKeyWasPressed(&joystick);
-		switch (key)
-		{
-		case 'a':
-			if (isMovementLegal(player.tipo, player.rotacion,
-								player.x - 1, player.y))
-			{
-				player.x--;
-			}
-			break;
-		case 'd':
-			if (isMovementLegal(player.tipo, player.rotacion,
-								player.x + 1, player.y))
-			{
-				player.x++;
-			}
-			break;
-		case 's':
-			if (isMovementLegal(player.tipo, player.rotacion,
-								player.x, player.y + 1))
-			{
-				player.y++;
-			}
-			else
-			{
-				storePieceInBoard(&player);
-				createNewTetramino(&player);
-			}
-			break;
-		case 'w':
-			if (isMovementLegal(player.tipo, (player.rotacion + 1) % 4,
-								player.x, player.y))
-			{
-				player.rotacion = (player.rotacion + 1) % 4;
-			}
-			break;
-		}
+        joyinfo_t joystick = {0, 0, J_NOPRESS};
+        joy_init();
 
-		// caida libre
-		time_t currentTime = time(NULL);
-		double elapsedTime = difftime(currentTime, startTime) * 1000;
+        do
+        {
+                // control
+                joystick = joy_read();
+                char key = whichKeyWasPressed(&joystick);
+                usleep(200000);
+                printf("%c\n", key);
+                switch (key)
+                {
+                case 'a':
+                        if (isMovementLegal(player.tipo, player.rotacion,
+                                                                player.x - 1, player.y))
+                        {
+                                player.x--;
+                        }
+                        break;
+                case 'd':
+                        if (isMovementLegal(player.tipo, player.rotacion,
+                                                                player.x + 1, player.y))
+                        {
+                                player.x++;
+                        }
+                        break;
+                case 's':
+                        if (isMovementLegal(player.tipo, player.rotacion,
+                                                                player.x, player.y + 1))
+                        {
+                                player.y++;
+                        }
+                        else
+                        {
+                                storePieceInBoard(&player);
+                                createNewTetramino(&player);
+                        }
+                        break;
+                case 'w':
+                        if (isMovementLegal(player.tipo, (player.rotacion + 1) % 4,
+                                                                player.x, player.y))
+                        {
+                                player.rotacion = (player.rotacion + 1) % 4;
+                        }
+                        break;
+                }
 
-		if (elapsedTime >= fallInterval)
-		{
-			startTime = currentTime;
+                // caida libre
+                time_t currentTime = time(NULL);
+                double elapsedTime = difftime(currentTime, startTime) * 1000;
 
-			if (isMovementLegal(player.tipo, player.rotacion,
-								player.x, player.y + 1))
-			{
-				player.y++;
-			}
-			else
-			{
-				storePieceInBoard(&player);
-				createNewTetramino(&player);
-			}
-		}
+                if (elapsedTime >= fallInterval)
+                {
+                        startTime = currentTime;
 
-		// rendering
-		updateScene(&player);
-		drawScene();
-		printf("\n");
+                        if (isMovementLegal(player.tipo, player.rotacion,
+                                                                player.x, player.y + 1))
+                        {
+                                player.y++;
+                        }
+                        else
+                        {
+                                storePieceInBoard(&player);
+                                createNewTetramino(&player);
+                        }
+                }
 
-		usleep(2000); // = 2 seg. no bajar de 2000 pq no renderea bn
+        /*      // rendering
+                updateScene(&player);
+                drawScene();
+                printf("\n"); */
 
-		eraseLineIfFull();
+                usleep(2000); // = 2 seg. no bajar de 2000 pq no renderea bn
 
-	} while (!isGameOver());
-	
-	puts("This is your end, cowboy...");
-	return 0;
+                eraseLineIfFull();
+
+        } while (!isGameOver());
+
+        puts("This is your end, cowboy...");
+        return 0;
 }
+
