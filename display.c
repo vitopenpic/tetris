b975b055af41fd1bcc999874394152a73072de5a
Vito <vitopensapiccolo@gmail.com>
2024-01-24 00:56:08 -0300

    added show score

--- display.c
+++ display.c
@@ -14,8 +14,9 @@
 
 #define N_PIECE_DIMX 4
 #define N_PIECE_DIMY 2
-#define N_PIECE_DISP_POSX 10 // posicion donde se mostrara la proxima 
-#define N_PIECE_DISP_POSY 1  // pieza en el display matricial
+
+#define DISPLAY_NUMX 3
+#define DISPLAY_NUMY 5
 
 // arreglo de las piezas a mostrar
 // en orden de: O, I, L, J, Z, S, T.
@@ -41,6 +42,69 @@ static const bool aNextPiece[7][N_PIECE_DIMY][N_PIECE_DIMX] =
 
 	{{0,0,1,0},
 	 {0,1,1,1}}
+};
+
+static const bool aDisplayNum[10][DISPLAY_NUMY][DISPLAY_NUMX] = 
+{
+	{{1,1,1}, // 0
+	 {1,0,1},
+	 {1,0,1},
+	 {1,0,1},
+	 {1,1,1}},
+
+	{{0,0,1}, // 1
+	 {0,0,1},
+	 {0,0,1},
+	 {0,0,1},
+	 {0,0,1}},
+
+	{{1,1,1}, // 2
+	 {0,0,1},
+	 {1,1,1},
+	 {1,0,0},
+	 {1,1,1}},
+
+	{{1,1,1}, // 3
+	 {0,0,1},
+	 {1,1,1},
+	 {0,0,1},
+	 {1,1,1}},
+
+	{{1,0,1}, // 4
+	 {1,0,1},
+	 {1,1,1},
+	 {0,0,1},
+	 {0,0,1}},
+
+	{{1,1,1}, // 5
+	 {1,0,0},
+	 {1,1,1},
+	 {0,0,1},
+	 {1,1,1}},
+
+	{{1,1,1}, // 6
+	 {1,0,0},
+	 {1,1,1},
+	 {1,0,1},
+	 {1,1,1}},
+
+	{{1,1,1}, // 7
+	 {0,0,1},
+	 {0,0,1},
+	 {0,0,1},
+	 {0,0,1}},
+
+	{{1,1,1}, // 8
+	 {1,0,1},
+	 {1,1,1},
+	 {1,0,1},
+	 {1,1,1}},
+
+	{{1,1,1}, // 9
+	 {1,0,1},
+	 {1,1,1},
+	 {0,0,1},
+	 {0,0,1}},
 };
 #endif 
 
@@ -101,35 +165,102 @@ void drawInTerminal(player_t *player)
 }
 
 #ifdef RASPI
-void drawInRaspberry(player_t *player)
+
+/* posicion con respecto al display de la raspi donde se mostrara 
+la proxima pieza */
+#define N_PIECE_DISP_POSX 10  
+#define N_PIECE_DISP_POSY 1  
+
+/* posicion de los numeros del puntaje con respecto al display de
+la raspi*/
+#define SCORE_THOUSAND_X 9
+#define SCORE_THOUSAND_Y 4
+#define SCORE_HUNDRED_X 13
+#define SCORE_HUNDRED_Y 4
+#define SCORE_TEN_X 9
+#define SCORE_TEN_Y 10
+#define SCORE_UNIT_X 13
+#define SCORE_UNIT_Y 10
+
+static void drawSceneRaspberry()
 {
-	// tablero, en la mitad izquierda del display 
-	// copia mScene al display   
-    for (int y = 0; y < BOARD_HEIGHT; y++)
+	dcoord_t p;	// hago esto pq no me deja pasar tipo 'disp_write({x, y}, ...)'	
+	for (int y = 0; y < BOARD_HEIGHT; y++)
     {
         for (int x = 0; x < BOARD_WIDTH; x++)
         {
-            if (getScene(x,y) == OCCUPIED)
-                disp_write({x, y}, D_ON);
+			p.x = x; p.y = y;            
+			if (getScene(x,y) == OCCUPIED)
+                disp_write(p, D_ON);
             else
-                disp_write({x, y}, D_OFF);
+                disp_write(p, D_OFF);
         }
     }
     disp_update();
+}
 
-	// proxima pieza y puntos/nivel en la otra mitad
-	// proxima pieza
+static void drawNextPieceRaspberry(player_t *player)
+{
+	dcoord_t p;	// hago esto pq no me deja pasar tipo 'disp_write({x, y}, ...)'	
 	for (int y1 = N_PIECE_DISP_POSY, y2 = 0; y2 < N_PIECE_DIMY; y1++, y2++)
 	{
 		for (int x1 = N_PIECE_DISP_POSX, x2 = 0; x2 < N_PIECE_DIMX; x1++, x2++)
 		{
+			p.x = x1; p.y = y1;			
 			if (aNextPiece[player->new_tipo][y2][x2] == OCCUPIED)
-				disp_write({x1, y1}, D_ON);
+				disp_write(p, D_ON);
 			else
-				disp_write({x1, y1}, D_OFF);
+				disp_write(p, D_OFF);
 		}	
 	}
-	
+}
+
+static void drawNumberToDisp(int x0, int y0, int num)
+{
+	dcoord_t p;	// hago esto pq no me deja pasar tipo 'disp_write({x, y}, ...)'	
+	for (int y1 = y0, y2 = 0; y2 < DISPLAY_NUM_Y; y1++, y2++)
+	{
+		for (int x1 = x0, x2 = 0; x2 < DISPLAY_NUM_X; x1++, x2++)
+		{
+			p.x = x1; p.y = y1;
+			if (aDispNum[num][y2][x2] == OCCUPIED)
+				disp_write(p, D_ON);
+			else 	
+				disp_write(p, D_OFF);			
+		}
+	}
+}
+
+static void	drawScoreRaspberry(player_t *player)
+{
+	if (player->score > 9999)
+	{
+		drawNumberToDisp(SCORE_UNIT_X, SCORE_UNIT_Y, 9);  
+		drawNumberToDisp(SCORE_TEN_X, SCORE_TEN_Y, 9);
+		drawNumberToDisp(SCORE_HUNDRED_X, SCORE_HUNDRED_Y, 9);
+		drawNumberToDisp(SCORE_THOUSAND_X, SCORE_THOUSAND_Y, 9);
+	}	
+
+	// muestra la unidad del puntaje	 
+	drawNumberToDisp(SCORE_UNIT_X, SCORE_UNIT_Y, player->score % 10);  
+	// muestra la decena del puntaje
+	drawNumberToDisp(SCORE_TEN_X, SCORE_TEN_Y, player->score % 100 / 10);
+	// muestra la centena del puntaje
+	drawNumberToDisp(SCORE_HUNDRED_X, SCORE_HUNDRED_Y, player->score % 1000 / 100);
+	// muestra el millar del puntaje
+	drawNumberToDisp(SCORE_THOUSAND_X, SCORE_THOUSAND_Y, player->score % 10000 / 1000);	
+}
+
+void drawInRaspberry(player_t *player)
+{
+	// tablero, en la mitad izquierda del display 
+	drawSceneRaspberry();  
+
+	// proxima pieza, arriba a la derecha
+	drawNextPieceRaspberry(player);
+		
+	// muestra el puntaje, abajo a la derecha 
+	drawScoreRaspberry(player);
 }	
 #endif
 
