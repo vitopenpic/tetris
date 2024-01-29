0127ca6947d661d8100481639513600d919572d1
Vito Pensa Piccolo <vitopensapiccolo@gmail.com>
2024-01-29 15:08:56 -0300

    trying for raspberry...

--- backend/menu.c
+++ backend/menu.c
@@ -1,11 +1,12 @@
 #include "menu.h"
+#include "../frontend/soundFX.h"
 #include <stdio.h>
 #include <stdlib.h>
 
-static menu_status_t status;		// estado de la accion a realizar por el menu
-static int status_index; 	/* indice que sirve para navegar 
-por el menu, una vez apretado enter se guarda su valor en status. uso int en vez de 
-menu_status_t por tema de aritmetica modular en navigateMenu() */
+static menu_status_t status; // estado de la accion a realizar por el menu
+static int status_index;	 /* indice que sirve para navegar
+ por el menu, una vez apretado enter se guarda su valor en status. uso int en vez de
+ menu_status_t por tema de aritmetica modular en navigateMenu() */
 
 menu_status_t menuStatus()
 {
@@ -29,19 +30,27 @@ void setMenuStatusOpen()
 }
 
 void navigateMenu(char key)
-{	
+{
 	switch (key)
-    {
-    case DOWN:
-		status_index = (status_index + 1) % 3;	
-		// OPEN no es una opcion a elegir, por eso el % 3	   
+	{ // OPEN no es una opcion a elegir, por eso el % 3
+	case DOWN:
+		status_index = (status_index + 1) % 3;
+#ifdef RASPI
+		playLockSound();
+#endif
 		break;
-    case UP:
-		status_index = (status_index - 1) % 3;        
+	case UP:
+		status_index = (status_index - 1) % 3;
+#ifdef RASPI
+		playLockSound();
+#endif
 		break;
-	case ENTER: 
+	case ENTER:
+#ifdef RASPI
+		playLineClearSound();
+#endif
 		status = abs(status_index); // status deja de valer OPEN
-		break;    
+		break;
 	}
 }
 
