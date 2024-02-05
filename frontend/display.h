478ee862861a6edd8961686c329ee35a238b5ab3
Vasco110 <129222033+Vasco110@users.noreply.github.com>
2024-02-04 21:07:49 -0300

    Update display.h

--- frontend/display.h
+++ frontend/display.h
@@ -15,6 +15,8 @@
 #include <allegro5/allegro_image.h>
 #include <allegro5/allegro.h>
 #include <allegro5/allegro_font.h>
+#include <allegro5/allegro_audio.h>
+#include <allegro5/allegro_acodec.h>
 #endif
 /**************************************************************************
  *                          FUNCIONES PARA TERMINAL
